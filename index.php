ae9b79235ddf61a82c6c340b10a37b70eebcb473
william-mateus <william.mtt003@gmail.com>
2022-07-10 14:17:05 -0300

    primeira versão da tela de longin

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Low Prices</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    
+    <img src="low price.png" alt="logo Low Price" class="logo">
+
+    <nav id="toobar">
+
+
+        <div style="margin-left: auto;">
+
+            <button type="button" id="cadastrar" class="button-toobar">Cadastrar-se</button>
+            <button type="button" id="login" class="button-toobar">login</button>
+        </div>
+
+    </nav>
+
+    <form onsubmit="" method="post">
+        <h1>Entrar</h1>
+       
+            <input type="text" placeholder="Nome" id="nome" class="input">
+        
+       
+        
+
+            <input type="password" placeholder="Senha" id="senha" class="input">
+
+        
+       
+        <button type = "submit" class="entrar">entrar</button>
+    </form>
+
+
+
+
+</body>
+
+</html>
